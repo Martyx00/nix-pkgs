899c136d2b358987f95fbf45ee7be304f9384e31
Martyx00 <martin.petran@protonmail.com>
2024-04-12 13:19:15 +0200

    Create default.nix

---
+++ theia/default.nix
@@ -0,0 +1,30 @@
+{ 
+  pkgs ? import <nixpkgs> { system = builtins.currentSystem; }
+}:
+with import <nixpkgs> {};
+with pkgs.makeDesktopItem;
+let 
+  icon_file = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/eclipse-theia/theia/master/logo/theia.svg";
+    sha256 = "sha256-2XEuO3mpjXsdX9ZNcJ2qgGvmlEw/DOvyKHnNDjwIzgY=";
+  };
+in
+  appimageTools.wrapType2 {
+    name = "Theia";
+    version = "1.48.300";
+    src = pkgs.fetchurl {
+      url = "https://www.eclipse.org/downloads/download.php?file=/theia/ide/latest/linux/TheiaIDE.AppImage&r=1";
+      sha256 = "sha256-Aw/Vv0wuJ4rmpOCPcbr9cABvfNANAD3IRYVGsZiWjdw=";
+    };
+    /* Desktop file and meta configuration go here */
+   extraInstallCommands = ''
+    mkdir -p $out/share/applications
+    echo "[Desktop Entry]
+    Type=Application
+    Name=Theia IDA
+    Exec=$out/bin/Theia
+    Terminal=false
+    Icon=${icon_file}" > $out/share/applications/theia.desktop
+  '';
+  
+}
